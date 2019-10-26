cb05b07291abf7bd6788579d7019386e86155243
Ervin <ervinxie@qq.com>
2019-10-26 15:09:48 +0800

    Naive Implementation of SKetchNaudio.

--- main.py
+++ main.py
@@ -221,6 +221,43 @@ def reshuffle():
     if request.method == 'GET':
         return "Do not support using GET to using recommendation. "
 
+audio_sketch_word = None
+audio_sketch_eng = None
+
+@app.route("/sketchNaudio", methods=['POST', 'GET'])
+def sketchNaudio():
+    if request.method == 'POST':
+        image_data = bytes(request.form.get('imgBase64'), encoding="ascii")
+        imgdata = base64.b64decode(image_data)
+        filename = './qs.png'
+        with open(filename, 'wb') as f:
+            f.write(imgdata)
+        start_time = time.time()
+        global audio_sketch_eng
+        global audio_sketch_word
+        if audio_sketch_word == '桌子':
+            audio_sketch_eng = 'table'
+        elif audio_sketch_word == '椅子':
+            audio_sketch_eng = 'chair'
+        results = sketch_search('./qs.png', 400, audio_sketch_eng)
+        end_time = time.time()
+        tmp = []
+        for i in results:
+            if i not in tmp:
+                tmp.append(i)
+                if len(tmp)>=20:
+                    break
+        results = tmp
+        #print(tmp)
+        print(results)
+
+        results = orm.query_model_by_names(results)
+        #print(results)
+        ret=[{"id":m.id,"name":m.name,"semantic":m.category.wordnetSynset,"thumbnail":"/thumbnail/%d"%(m.id,)} for m in results]
+        print("\r\n\r\n------- %s secondes --- \r\n\r\n" % (end_time - start_time))
+        return json.dumps(ret)
+    return "Post image! "
+
 @app.route('/toy_uploader', methods=['GET', 'POST'])
 def toy_uploader():
     uuid4 = uuid.uuid4()
@@ -256,8 +293,12 @@ def toy_uploader():
     result = client.asr(get_file_content(filename), 'wav', 16000, {
         'dev_pid': 1536,
     })
+    global audio_sketch_word
+    global audio_sketch_eng
+    if 'result' in result:
+        audio_sketch_word = result['result'][0]
 
-    print(result)
+    print(audio_sketch_word)
     os.remove(filename)
     return json.dumps(result)
 
