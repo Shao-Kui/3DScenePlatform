c8913434835c5bf45111fe510ddfef59e761a1c8
Ervin <ervinxie@qq.com>
2020-01-18 16:43:19 +0800

    start from Adding latent space stub for Wei-Yu.

--- static/js/render.js
+++ static/js/render.js
@@ -1,21 +1,21 @@
 var directionalLight;
 var ambientLight;
 
-var render_initialization = function(){
-  var axis = new THREE.AxesHelper(1000);
-  scene.add(axis);
+var render_initialization = function () {
+    var axis = new THREE.AxesHelper(1000);
+    scene.add(axis);
 
-  directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.3);
-  directionalLight.castShadow = true;
-  directionalLight.position.y = 0.5;
-  scene.add(directionalLight);
+    directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.3);
+    directionalLight.castShadow = true;
+    directionalLight.position.y = 0.5;
+    scene.add(directionalLight);
 
-  var ambientLight=new THREE.AmbientLight(0xFFFFFF, 0.9);
-  scene.add(ambientLight);
+    var ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.9);
+    scene.add(ambientLight);
 }
 
-var render_update = function(){
-  var time= Date.now() * 0.0001;
-  //directionalLight.position.x = Math.sin(time);
-  //directionalLight.position.z = Math.cos(time);
+var render_update = function () {
+    var time = Date.now() * 0.0001;
+    //directionalLight.position.x = Math.sin(time);
+    //directionalLight.position.z = Math.cos(time);
 }
