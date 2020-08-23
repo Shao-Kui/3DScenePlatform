ffa8d72496d0ea5ea276c95f66b7dfcb7ea06753
Shao-Kui <ezioshaokui@qq.com>
2020-08-23 12:09:10 +0800

    More advanced auto-topview.

--- static/js/QE.js
+++ static/js/QE.js
@@ -18,8 +18,16 @@ let topdownview = function(){
   let lx = (bbox.max[0] + bbox.min[0]) / 2;
   let ymax = bbox.max[1];
   let lz = (bbox.max[2] + bbox.min[2]) / 2;
+  let camfovratio = Math.tan((camera.fov/2) * Math.PI / 180); 
+  let height_x = (bbox.max[0]/2 - bbox.min[0]/2) / camfovratio;
+  let height_z = (bbox.max[2]/2 - bbox.min[2]/2) / camfovratio;
+  let camHeight = ymax + Math.max(height_x, height_z)
+  if(camHeight > 36 || camHeight < -36 || isNaN(camHeight)){
+    // prevent if there is NaN in datasets; 
+    camHeight = 6;
+  }
   camera.rotation.order = 'YXZ';
-  camera.position.set(lx, ymax + 1, lz);
+  camera.position.set(lx, camHeight, lz);
   camera.lookAt(lx, 0, lz);
   orbitControls.target.set(lx, 0, lz);
 };
