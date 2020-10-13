f7f5c6f681165ecf16fa217016dcee76ea63104f
Shao-Kui <ezioshaokui@qq.com>
2020-10-13 16:59:56 +0800

    Rendering Improvement.

--- static/js/render.js
+++ static/js/render.js
@@ -22,7 +22,8 @@ var skyMaterialsCloudtop = [
     new THREE.MeshBasicMaterial({map: tgaLoader.load("/static/skybox/ely_cloudtop/cloudtop_lf.tga"), side: THREE.DoubleSide})
 ];
 
-var spotLight = new THREE.SpotLight( 0xffffff, 2);
+var spotLight = new THREE.SpotLight( 0xffffff, 6);
+var directionalLight = new THREE.DirectionalLight(0xFFFFFF, 9.3);
 
 var render_initialization = function () {
     // var axis = new THREE.AxesHelper(1000);
@@ -34,26 +35,25 @@ var render_initialization = function () {
 
     //lighting and shadowing
     //0.9 is the stable lightness of ambient light after mid-term; 
-    var ambientLight = new THREE.AmbientLight(0xFFFFFF, 0.6);
-    //scene.add(ambientLight);
+    var ambientLight = new THREE.AmbientLight(0xFFFFFF, 3.6);
+    scene.add(ambientLight);
 
     //0.3 is the stable lightness of directional light after mid-term; 
-    var directionalLight = new THREE.DirectionalLight(0xFFFFFF, 0.3);
-    directionalLight.castShadow = false;
+    directionalLight.castShadow = true;
     directionalLight.position.set(0, 10, 0);
-    directionalLight.shadow.mapSize.width = 4096 * 4;
-    directionalLight.shadow.mapSize.height = 4096 * 4;
+    directionalLight.shadow.mapSize.width = 4096;
+    directionalLight.shadow.mapSize.height = 4096;
     directionalLight.shadow.camera.near = 0.01;
     directionalLight.shadow.camera.far = 100;
     directionalLight.shadow.camera.left = -100;
     directionalLight.shadow.camera.right = 100;
     directionalLight.shadow.camera.top = 100;
     directionalLight.shadow.camera.bottom = -100;
-    // scene.add(directionalLight);
-    // scene.add(directionalLight.target)
+    scene.add(directionalLight);
+    scene.add(directionalLight.target)
 
     //Create a PointLight and turn on shadows for the light
-    spotLight.angle = Math.PI / 4;
+    spotLight.angle = Math.PI / 3;
     spotLight.penumbra = 0;
     spotLight.decay = 0;
     spotLight.distance = 0;
@@ -66,7 +66,7 @@ var render_initialization = function () {
     scene.add( spotLight.target );
 
     // add HemisphereLight
-    let hlight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 8.4 );
+    let hlight = new THREE.HemisphereLight( 0xffffff, 0xffffff, 2.4 );
     scene.add( hlight );
 
     //adding skybox
@@ -78,9 +78,11 @@ var render_initialization = function () {
 
 
 var render_update = function () {
-    // renderTime = Date.now() * 0.00001;
+    //renderTime = Date.now() * 0.00001;
     spotLight.position.x = lx_level + Math.sin(renderTime) * 12;
     spotLight.position.z = lz_level + Math.cos(renderTime) * 12;
+    //directionalLight.position.x = Math.sin(renderTime);
+    //directionalLight.position.z = Math.cos(renderTime);
     skyBox.rotation.y = (renderTime % (Math.PI * 2)) + Math.PI / 2;
 };
 
