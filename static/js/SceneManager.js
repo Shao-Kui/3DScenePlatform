3a1157a0723e746834f8f88f01d0cb5e8a79393f
Ervin <ervinxie@qq.com>
2022-01-17 15:31:25 +0800

    movable wall

--- static/js/SceneManager.js
+++ static/js/SceneManager.js
@@ -34,6 +34,7 @@ class SceneManager {
         this.fCache = []; 
         this.wfCache = []; 
         this.wCache = []; 
+        this.newWallCache = []; 
         this.init_canvas();
     }
 
@@ -134,6 +135,12 @@ class SceneManager {
         this.fCache = []; 
         this.wfCache = []; 
         this.wCache = []; 
+        this.newWallCache = []; 
+        this.useNewWall = false;
+        this.loadingManager = new THREE.LoadingManager(() => {
+            if (this.useNewWall)
+                this.reconstructWalls();
+        });
         var self = this;
         for (var i = 0; i < this.scene_json.rooms.length; i++) {
             self.load_cwf_room_meta(this.scene_json.rooms[i])
@@ -157,7 +164,7 @@ class SceneManager {
     load_cwf_instances = (modelId, suffix, roomId) => {
         var meta = modelId + suffix;
         var self = this;
-        var objLoader = new THREE.OBJLoader();
+        var objLoader = new THREE.OBJLoader(this.loadingManager);
         let mtl_path = "/room/" + self.scene_json.origin + "/" + meta + '.mtl';
         let obj_path = "/room/" + self.scene_json.origin + "/" + meta + '.obj';
         objLoader.load(obj_path, function (instance) {
@@ -167,9 +174,10 @@ class SceneManager {
             instance.receiveShadow = true;
             instance.name = meta;
             traverseObjSetting(instance);
-            self.scene.add(instance);
+            // self.scene.add(instance);
             if(suffix === 'f'){
                 instance.traverse(function(child){
+                    self.scene.add(instance);
                     if(child instanceof THREE.Mesh){
                         // child.material.color.setHex(0x8899AA);
                         // child.material.map = texture;
@@ -186,8 +194,11 @@ class SceneManager {
                         // child.material.color.setHex(0x8899AA);
                     }
                 });
-                self.cwfCache.push(instance);
-                self.wfCache.push(instance);
+                if (self.useNewWall == false) {
+                    self.scene.add(instance);
+                    self.cwfCache.push(instance);
+                    self.wfCache.push(instance);
+                }
                 self.wCache.push(instance); 
             }
             instance
@@ -283,6 +294,293 @@ class SceneManager {
         this.camera.updateProjectionMatrix();
         this.renderer.setSize(this.canvas.width, this.canvas.height);
     };
+    
+    addWall = (walls, coor, s, t) => {
+        let keyNotExist = true;
+        for (const key in walls) {
+            if (Math.abs(coor - key) > 0.001) continue;
+            else keyNotExist = false;
+            let seg = walls[key];
+            seg.push([s, t, false]);
+            seg.sort((a, b) => a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+            for (let i = 1; i < seg.length;) {
+                if (seg[i][1] <= seg[i - 1][1]) {
+                    seg.splice(i, 1);
+                } else if (seg[i][0] <= seg[i - 1][1] + 0.241) {
+                    seg[i - 1][1] = seg[i][1];
+                    seg.splice(i, 1);
+                } else {
+                    i++;
+                }
+            }
+        }
+        if (keyNotExist) {
+            // coor = Math.round(coor*10000)/10000;
+            walls[coor] = [[s, t, false]];
+        }
+    }
+
+    addEdge = (v1, v2, norm) => {
+        if (v1[0] == v2[0]) {
+            let s = Math.min(v1[1], v2[1]);
+            let t = Math.max(v1[1], v2[1]);
+            if (norm[0] < 0)
+                this.addWall(this.walls.x[0], v1[0], s, t);
+            else
+                this.addWall(this.walls.x[1], v1[0], s, t);
+        }
+        if (v1[1] == v2[1]) {
+            let s = Math.min(v1[0], v2[0]);
+            let t = Math.max(v1[0], v2[0]);
+            if (norm[1] < 0)
+                this.addWall(this.walls.z[0], v1[1], s, t);
+            else
+                this.addWall(this.walls.z[1], v1[1], s, t);
+        }
+    }
+
+    genWallMesh = (x, y, z, py) => {
+        const material = new THREE.MeshBasicMaterial();
+        let geometry = new THREE.BoxGeometry(x, y, z);
+        // geometry.translate(px, py, pz);
+        let newWall = new THREE.Mesh(geometry, material);
+        newWall.position.setY(py);
+        return newWall;
+    }
+
+    addWallGroup = (axis, k0, k1, w) => {
+        w.splice(2, 1);
+        let groupId = this.wallGroup.length;
+        let wg = { "axis": axis, "groupId": groupId, "coor": [k0, k1], "halfWidth": (k1-k0)/2, "seg": w, "x": [], "z": [], "wd": [], "walls": [], "idxRange": [], "adjFloor": [], "adjWall": [], "adjRoomShape": [] };
+        if (axis == "x") {
+            wg.x = [k0, k1];
+            wg.z = w;
+        }
+        else {
+            wg.x = w;
+            wg.z = [k0, k1];
+        }
+        this.wallGroup.push(wg);
+    }
+
+    reconstructWalls = () => {
+        var self = this;
+        this.walls = { x: [{}, {}], z: [{}, {}] };
+        this.wallGroup = [];
+
+        const rooms = this.scene_json.rooms;
+        rooms.forEach(room => {
+            let roomShape = room.roomShape;
+            if (roomShape == undefined) return;
+            let max = [...roomShape[0]], min = [...roomShape[0]];
+            for (let i = 0; i < roomShape.length; ++i) {
+                let j = (i + 1) == roomShape.length ? 0 : i + 1;
+                self.addEdge(roomShape[i], roomShape[j], room.roomNorm[i]);
+                max[0] = Math.max(max[0], roomShape[i][0]);
+                max[1] = Math.max(max[1], roomShape[i][1]);
+                min[0] = Math.min(min[0], roomShape[i][0]);
+                min[1] = Math.min(min[1], roomShape[i][1]);
+            }
+            room["roomShapeBBox"] = {"max": max, "min": min}
+        });
+
+        for (const axis of ["x", "z"]) {
+            let wall0 = this.walls[axis][0];
+            let wall1 = this.walls[axis][1]
+            let key0 = Object.keys(wall0);
+            let key1 = Object.keys(wall1);
+            key0.sort((a, b) => parseFloat(a) - parseFloat(b));
+            key1.sort((a, b) => parseFloat(a) - parseFloat(b));
+            key0.forEach((val,idx)=>key0[idx]=parseFloat(val));
+            key1.forEach((val,idx)=>key1[idx]=parseFloat(val));
+            for (let k0 of key0) {
+                let seg0 = [...wall0[k0]];
+                for (let k1 of key1) {
+                    if (k0 > k1) continue;
+                    if (k0 + 0.25 < k1) break;
+                    let tmpSeg=[];
+                    for (let i = 0; i < seg0.length; ++i) {
+                        let w0 = wall0[k0][i];
+                        for (let w1 of wall1[k1]) {
+                            if (w0[0]>w1[1] || w0[1]<w1[0]) continue;
+                            tmpSeg.push([Math.min(w0[0], w1[0]), Math.max(w0[1], w1[1])]);
+                            seg0.splice(i, 1);
+                            w1[2] = true;
+                            break;
+                        }
+                    }
+                    tmpSeg.sort((a, b) => a[0] == b[0] ? a[1] - b[1] : a[0] - b[0]);
+                    for (let i = 1; i < tmpSeg.length;) {
+                        if (tmpSeg[i][1] <= tmpSeg[i - 1][1]) {
+                            tmpSeg.splice(i, 1);
+                        } else if (tmpSeg[i][0] <= tmpSeg[i - 1][1] + 0.001) {
+                            tmpSeg[i - 1][1] = tmpSeg[i][1];
+                            tmpSeg.splice(i, 1);
+                        } else {
+                            i++;
+                        }
+                    }
+                    
+                    for (let w of tmpSeg)
+                        this.addWallGroup(axis, k0, k1, w);
+                }
+                
+                for (let w0 of seg0)
+                    this.addWallGroup(axis, k0, k0+0.24, w0);
+            }
+            for (let k1 of key1)
+                for (let w1 of wall1[k1])
+                    if (!w1[2])
+                        this.addWallGroup(axis, k1 - 0.24, k1, w1);
+        }
+
+        for (let wg of this.wallGroup) {
+            for (let room of rooms) {
+                let bbox = room.roomShapeBBox;
+                if (bbox == undefined) continue;
+                if (bbox.min[0] > wg.x[1] || bbox.max[0] < wg.x[0] || bbox.min[1] > wg.z[1] || bbox.max[1] < wg.z[0])
+                    continue;
+                let pIdList = [[], []];
+                let roomShape = room.roomShape;
+                for (let i = 0; i < roomShape.length; ++i) {
+                    let x = roomShape[i][0], z = roomShape[i][1];
+                    if (wg.x[0] <= x && x <= wg.x[1] && wg.z[0] <= z && z <= wg.z[1]) {
+                        if (wg.axis == "x") {
+                            if (Math.abs(x-wg.x[0])<0.001)
+                                pIdList[0].push(i);
+                            else if (Math.abs(x-wg.x[1])<0.001)
+                                pIdList[1].push(i);
+                        } else {
+                            if (Math.abs(z-wg.z[0])<0.001)
+                                pIdList[0].push(i);
+                            else if (Math.abs(z-wg.z[1])<0.001)
+                                pIdList[1].push(i);
+                        }
+                    }
+                }
+                if (pIdList[0].length || pIdList[1].length) {
+                    wg.adjRoomShape.push([room.roomId, pIdList]);
+                    room.objList.forEach(obj => {
+                        let bbox = obj.bbox;
+                        if (obj.coarseSemantic == "Window" || obj.coarseSemantic == "Door")
+                            if (!(bbox.min[0] > wg.x[1] || bbox.max[0] < wg.x[0] || bbox.min[2] > wg.z[1] || bbox.max[2] < wg.z[0] || bbox.min[1]==bbox.max[1])) {
+                                if (wg.axis == "x")
+                                    wg.wd.push([bbox.min[2], bbox.max[2], bbox.min[1], bbox.max[1]]);
+                                else
+                                    wg.wd.push([bbox.min[0], bbox.max[0], bbox.min[1], bbox.max[1]]);
+                            }
+                    });
+                }
+            }
+            if (wg.wd.length == 0) {
+                wg.walls.push(wg.seg);
+            } else {
+                wg.wd.sort((a, b)=>a[0]-b[0]);
+                wg.walls.push([wg.seg[0], wg.wd[0][0]]);
+                for (let i = 0; i < wg.wd.length; ++i) {
+                    wg.walls.push(wg.wd[i]);
+                    if (i < wg.wd.length - 1)
+                        wg.walls.push([wg.wd[i][1], wg.wd[i+1][0]]);
+                    else
+                        wg.walls.push([wg.wd[i][1], wg.seg[1]]);
+                }
+            }
+        }
+
+        for (let groupId = 0; groupId < this.wallGroup.length; ++groupId) {
+            let wg = this.wallGroup[groupId];
+            let ls = this.newWallCache.length;
+            let axis = wg.axis;
+            let x, z, px, pz;
+            for (let w of wg.walls) {
+                let curIdx = this.newWallCache.length;
+                if (axis == "x") {
+                    x = wg.x[1] - wg.x[0];
+                    z = w[1] - w[0];
+                    px = (wg.x[1] + wg.x[0]) / 2;
+                    pz = (w[1] + w[0]) / 2;
+                } else {
+                    x = w[1] - w[0];
+                    z = wg.z[1] - wg.z[0];
+                    px = (w[1] + w[0]) / 2;
+                    pz = (wg.z[1] + wg.z[0]) / 2;
+                }
+                let instance = new THREE.Group();
+                if (w.length == 2) {
+                    instance.add(this.genWallMesh(x, 2.6, z, 1.3));
+                    instance.userData = {"type": "w", "axis": axis, "groupId": groupId, "index": curIdx};
+                } else {
+                    instance.add(this.genWallMesh(x, 2.6-w[3], z, (2.6-w[3])/2+w[3]));
+                    instance.add(this.genWallMesh(x, w[2], z, w[2] / 2));
+                    instance.userData = {"type": "d", "axis": axis, "groupId": groupId, "index": curIdx};
+                }
+                instance.position.setX(px);
+                instance.position.setZ(pz);
+                this.scene.add(instance);
+                this.newWallCache.push(instance);
+                this.cwfCache.push(instance);
+                this.wfCache.push(instance);
+                this.wCache.push(instance); 
+            }
+            let lt = this.newWallCache.length;
+            wg.idxRange = [ls, lt];
+            
+            let adjFloor = []
+            for (let f = 0; f < this.fCache.length; ++f) {
+                let pIdList = [[], []];
+                let instance = this.fCache[f];
+                instance.traverse(function (child) {
+                    if (child instanceof THREE.Mesh) {
+                        let attr = child.geometry.attributes;
+                        let pos = attr.position.array;
+                        for (let i = axis == "x" ? 0 : 2; i < pos.length; i += 3) {
+                            let q = axis == "x" ? pos[i + 2] : pos[i - 2];
+                            if (q < wg.seg[0] - 0.25 || q > wg.seg[1] + 0.25) continue;
+                            if (Math.abs(pos[i] - wg.coor[0]) < 0.001) {
+                                pIdList[0].push(i);
+                            } else if (Math.abs(pos[i] - wg.coor[1]) < 0.001) {
+                                pIdList[1].push(i);
+                            }
+                        }
+                    }
+                });
+                if (pIdList[0].length || pIdList[1].length)
+                    adjFloor.push([f, pIdList]);
+            }
+            wg.adjFloor = adjFloor;
+        }
+
+        for (let wg of this.wallGroup) {
+            let adjWall = [];
+            let axis = wg.axis;
+            let coor = wg.coor;
+            for (let w = 0; w < this.newWallCache.length; ++w) {
+                let instance = this.newWallCache[w];
+                if (instance.userData.axis == axis) continue;
+                if (instance.userData.type == "d") continue;
+                let pIdList = [[], []];
+                let pos = instance.children[0].geometry.attributes.position.array;
+                let offsetp = axis == "x" ? instance.position.x : instance.position.z;
+                let offsetq = axis == "x" ? instance.position.z : instance.position.x;
+                for (let i = axis == "x" ? 0 : 2; i < pos.length; i += 3) {
+                    let q = axis == "x" ? pos[i + 2] : pos[i - 2];
+                    if (offsetq + q < wg.seg[0]-0.25 || offsetq + q > wg.seg[1]+0.25) continue;
+                    if (Math.abs(offsetp + pos[i] - coor[0])<0.001) {
+                        pIdList[0].push(i);
+                    } else if (Math.abs(offsetp + pos[i] - coor[1])<0.001) {
+                        pIdList[1].push(i);
+                    }
+                }
+                if (pIdList[0].length || pIdList[1].length) {
+                    adjWall.push([w, pIdList]);
+                }
+            }
+            wg.adjWall = adjWall;
+        }
+        let geometry = new THREE.BoxGeometry(100, 0, 100);
+        const material = new THREE.MeshBasicMaterial();
+        this.infFloor = new THREE.Mesh(geometry, material);
+    }
 }
 
 class SceneController {
